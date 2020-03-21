1b5b7ab5ad8e806b2d79143045743fd7859b6b24
MaGaroo <amoomajid99@gmail.com>
2020-03-21 11:05:21 +0430

    Add post model to blog app

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext as _
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=100,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    author = models.CharField(
+        verbose_name=_('Author'),
+        max_length=100,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+
+    text = models.TextField(
+        null=False,
+        blank=False,
+        max_length=100000,
+    )
