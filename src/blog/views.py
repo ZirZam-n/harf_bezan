eafce52aa108daeabd7b8b9b59f14c5cd48a9cd5
MaGaroo <amoomajid99@gmail.com>
2020-03-21 11:35:13 +0430

    Add base of blog view

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Post
+
+
+def all_posts_view(request):
+    posts_list = Post.objects.all()
+    return render(
+        request,
+        "blog/index.html",
+        {'posts_list': posts_list}
+    )
