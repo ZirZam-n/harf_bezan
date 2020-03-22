209e3de1b15eb89e6efdac046be1acece635ad0a
MaGaroo <amoomajid99@gmail.com>
2020-03-22 09:56:22 +0430

    Add form rendering to new post view and modify new post page to send data to the backend

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
-from blog.models import Post
+
+from .forms import PostForm
+from .models import Post
 
 
 def all_posts_view(request):
@@ -14,7 +17,19 @@ def all_posts_view(request):
 
 
 def new_post_view(request):
-    return render(
-        request,
-        "blog/new_post.html",
-    )
+    if request.method == 'GET':
+        return render(
+            request,
+            "blog/new_post.html",
+        )
+    elif request.method == 'POST':
+        print(request.POST)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect(reverse('index'))
+        return render(
+            request,
+            "blog/new_post.html",
+            {'errors': form.errors},
+        )
