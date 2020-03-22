f401ccfa57732152e5fe3a6357e3e7dc2fbd30c7
MaGaroo <amoomajid99@gmail.com>
2020-03-22 09:09:31 +0430

    Add static files of new post page

--- src/static/js/renderer.js
+++ src/static/js/renderer.js
@@ -2,3 +2,17 @@ var converter = new showdown.Converter();
 
 [].forEach.call(document.getElementsByClassName('post-text'),
     (e) => e.innerHTML = converter.makeHtml(e.innerHTML.trim()));
+
+editor = document.getElementById('editor');
+preview_box = document.getElementById('preview');
+
+function show_preview() {
+    preview_box.innerHTML = converter.makeHtml(editor.value.trim());
+}
+
+if (editor && preview_box)
+    editor.addEventListener('keyup', show_preview);
+
+$(document).ready( function() {
+    $("input[type='text'], textarea").attr('spellcheck',false);
+});
